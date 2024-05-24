215cd29d21d14d721dc9b5d7a7b8c07d12392437
Heet Doshi <doshiheet2872003@gmail.com>
2024-05-24 14:49:34 +0530

    Angular 18 used!

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
+    provideAnimations(),
     importProvidersFrom(HttpClientModule),
     importProvidersFrom(BrowserAnimationsModule), // Import BrowserAnimationsModule here
     MessageService
   ]
 };
+
